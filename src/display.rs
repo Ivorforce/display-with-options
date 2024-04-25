deb4c6bb5dd8225341ca85f0ee7e284c2f5bc358
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-04-25 20:55:28 +0200

    Remove unneeded format_with_options function.

--- src/display.rs
+++ src/display.rs
@@ -1,10 +1,5 @@
 use std::fmt::{Display, Formatter};
-use crate::with_options;
 
 pub trait DisplayWithOptions<Options> {
     fn fmt(&self, f: &mut Formatter, options: &Options) -> std::fmt::Result;
-}
-
-pub fn format_with_options<Options>(object: &impl DisplayWithOptions<Options>, options: &Options) -> String {
-    format!("{}", with_options(object, options))
 }
